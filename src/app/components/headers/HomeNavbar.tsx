810b4e5631d2bce5161791eec64ae8992ff3ae56
DEEN <studiousdoston@gmail.com>
2026-07-02 07:54:35 +0900

    feat: homeNavbar menu part refactoring

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -5,32 +5,14 @@ export function HomeNavbar() {
   const authMember = null;
   return (
     <main className="home-navbar">
-      <Container sx={{ mt: "55px", height: "642px" }}>
-        <Stack
-          sx={{
-            height: "50px",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-          direction={"row"}
-        >
+      <Container className="navbar-container">
+        <Stack className="menu">
           <Box>
             <NavLink to={"/"}>
-              <img
-                style={{ width: "125px", height: "30px" }}
-                src="/icons/burak.svg"
-                alt="logo"
-              />
+              <img className="brand-logo" src="/icons/burak.svg" alt="logo" />
             </NavLink>
           </Box>
-          <Stack
-            direction={"row"}
-            sx={{
-              justifyContent: "space-between",
-              minWidth: "700px",
-              alignItems: "center",
-            }}
-          >
+          <Stack className="links">
             <Box className={"hover-line"}>
               <NavLink to={"/"} activeClassName="underline">
                 Home
@@ -63,15 +45,17 @@ export function HomeNavbar() {
             {/* BASKET*/}
             {!authMember ? (
               <Box className={"hover-line"}>
-                <Button
-                  variant="contained"
-                  style={{ background: "#3776cc", color: " #f8f8ff" }}
-                >
+                <Button variant="contained" className="login-button">
                   Login
                 </Button>
               </Box>
             ) : (
-              <img alt="img" />
+              <img
+                className="user-avatar"
+                src={"/icons/default-user.svg"}
+                aria-haspopup={"true"}
+                alt="img"
+              />
             )}
           </Stack>
         </Stack>
