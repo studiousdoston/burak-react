50850b88da29451ae7a436929224894a15de2edc
DEEN <studiousdoston@gmail.com>
2026-07-21 11:26:52 +0900

    fix: modify homePage screen component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
 import HomePage from "./screens/homePage";
 import ProductsPage from "./screens/productsPage";
@@ -11,7 +10,6 @@ import HelpPage from "./screens/helpPage";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
-import Test from "./screens/Test";
 
 function App() {
   const location = useLocation();
@@ -33,7 +31,7 @@ function App() {
           <HelpPage />
         </Route>
         <Route path="/">
-          <Test />
+          <HomePage />
         </Route>
       </Switch>
       <Footer />
